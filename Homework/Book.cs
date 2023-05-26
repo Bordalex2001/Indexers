63d98573bb160c792d38f3ac6a59f91f3cf03bfd
Bord_bf73@student.itstep.org <Bord_bf73@student.itstep.org>
2023-05-27 00:02:51 +0300

    Add Homework project

---
+++ Homework/Book.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework
+{
+    internal class Book
+    {
+        private string name; //название книги
+        private string author; //автор книги
+        private int year; //год издания
+        public Book()
+        {
+            name = null;
+            author = null;
+            year = 0;
+        }
+        public Book(string name, string author, int year)
+        {
+            this.name = name;
+            this.author = author;
+            this.year = year;
+        }
+        public string Name
+        {
+            get { return name; } set { name = value; }
+        }
+        public string Author
+        {
+            get { return author; } set { author = value; }
+        }
+        public int Year
+        {
+            get { return year; } set { year = value; }
+        }
+        public override string ToString()
+        {
+            return $"–{name} by {author}, {year}";
+        }
+    }
+    internal class BookList
+    { 
+        private List<Book> books; //список книг
+        public BookList()
+        { 
+            books = new List<Book>(); 
+        }
+        public BookList(List<Book> books)
+        { 
+            this.books = books; 
+        }
+        public List<Book> Books
+        {
+            get { return books; } set { books = value; }
+        }
+        public Book this[int index] //поиск и замена книги по индексу
+        {
+            get //поиск
+            { 
+                if (index >= 0 && index < books.Count)
+                {
+                    return books[index];
+                }
+                else
+                {
+                    throw new Exception("Book not found by this index.");
+                }
+            } 
+            set //замена
+            { 
+                if (index >= 0 && index < books.Count) 
+                {
+                    books[index] = value;
+                }
+                else
+                {
+                    throw new Exception("Unable to change book data by this index.");
+                }
+            }
+        }
+        public Book this[string name] //поиск книги по названию
+        {
+            get
+            {
+                foreach (Book book in books)
+                {
+                    if (book.Name == name)
+                    {
+                        return book;
+                    }
+                }
+                throw new Exception("Book not found by the name.");
+            }
+        }
+        public Book this[string author, int year] //поиск книги по автору и году издания
+        {
+            get
+            {
+                foreach (Book book in books)
+                {
+                    if (book.Year == year && book.Author == author)
+                    {
+                        return book;
+                    }
+                }
+                throw new Exception("Book not found by the author and the year.");
+            }
+        }
+        public override string ToString()
+        {
+            return books.ToString();
+        }
+        public BookList AddBook(Book book) //Добавление книги
+        {
+            books.Add(book);
+            return this;
+        }
+        public BookList RemoveBook(Book book) //Удаление книги
+        {
+            books.Remove(book);
+            return this;
+        }
+        public static BookList operator+(BookList bookList, Book book)
+        {
+            bookList.AddBook(book);
+            return bookList;
+        }
+        public static BookList operator-(BookList bookList, Book book)
+        {
+            bookList.RemoveBook(book);
+            return bookList;
+        }
+    }
+}
---
+++ Homework/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Class BookList");
+            BookList book_list = new BookList();
+            Book book_1 = new Book("The Picture of Dorian Gray", "Oscar Wilde", 1890);
+            Book book_2 = new Book("The Da Vinci Code", "Dan Brown", 2003);
+            Book book_3 = new Book("Wish You Were Here", "Jodi Picoult", 2021);
+            Book book_4 = new Book("The Improbability of Love", "Hannah Rothschild", 2015);
+            Book book_5 = new Book("The Judgement of Strangers", "Andrew Taylor", 1997);
+            book_list += book_1;
+            book_list += book_2;
+            book_list += book_3;
+            book_list += book_4;
+            book_list += book_5;
+            Console.WriteLine("Book list for reading:");
+            for (int i = 0; i < book_list.Books.Count; i++)
+            {
+                Console.WriteLine(book_list[i]);
+            }
+            Console.WriteLine("\nBook search by index: " + book_list[1]);
+            Console.WriteLine("Book search by name: " + book_list["The Improbability of Love"]);
+            Console.WriteLine("Book search by author and year: " + book_list["Jodi Picoult", 2021]);
+            Console.WriteLine("\nBook list after changing books:");
+            book_list[0] = new Book("Van Gogh: The Life", "Steven Naifeh", 2011);
+            book_list[3] = new Book("Edward Munch: 1863-1944", "Ulrich Bischoff", 1990);
+            for (int i = 0; i < book_list.Books.Count; i++)
+            {
+                Console.WriteLine(book_list[i]);
+            }
+            book_list -= book_3;
+            book_list -= book_5;
+            Console.WriteLine("\nBook list after removing books:");
+            for (int i = 0; i < book_list.Books.Count; i++)
+            {
+                Console.WriteLine(book_list[i]);
+            }
+        }
+    }
+}
